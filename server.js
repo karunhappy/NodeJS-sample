6dab1e28d7cd49aa4e7fa8229b99035f9fbbabcb
karun.happy358@gmail.com <karun_happy358@yahoo.com>
2018-10-03 17:44:24 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        headTitle: 'Home Page head title',
+        pageTitle: 'Welcome to Express'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page Sample'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        code: 404,
+        errorMessage: 'Page not found. Try visit here.'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000.');
+});
